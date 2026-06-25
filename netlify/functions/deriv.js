095eb172a6b15390bb69a4afd647492b18ec8c7d
yuttanafx <74797861+yuttanafx@users.noreply.github.com>
2026-06-25 07:24:58 +0700

    Create deriv.js

---
+++ netlify/functions/deriv.js
@@ -0,0 +1,41 @@
+exports.handler = async function(event, context) {
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'POST, OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type'
+  };
+
+  if (event.httpMethod === 'OPTIONS') {
+    return { statusCode: 200, headers, body: '' };
+  }
+
+  if (event.httpMethod !== 'POST') {
+    return {
+      statusCode: 405,
+      headers,
+      body: JSON.stringify({ error: 'Method not allowed' })
+    };
+  }
+
+  try {
+    const response = await fetch('https://ws.binaryws.com/websockets/v3', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: event.body
+    });
+
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify(data)
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      headers,
+      body: JSON.stringify({ error: 'Proxy failed', message: error.message })
+    };
+  }
+};
